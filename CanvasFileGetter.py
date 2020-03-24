0d343307c6469518a3c1b5da3b20ccf6384fbba1
Sharkgrammer <danielkeanekelly@gmail.com>
2020-03-24 18:44:58 +0000

    Fix for courses with hidden links

--- CanvasFileGetter.py
+++ CanvasFileGetter.py
@@ -3,7 +3,10 @@ import shutil
 from tkinter import *
 from tkinter.ttk import Progressbar
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 import threading
+
+from selenium.webdriver.support.wait import WebDriverWait
 
 splitter = ":@:"
 dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -34,9 +37,12 @@ def courseScanner(innerUrl, name, amt):
 
     linkPercent = coursePercent * (1 / len(itemsStrings))
     for string in itemsStrings:
-        string = str(string)
-        fileScanner(string.split(splitter)[0], linkCleanse(name + "\\" + string.split(splitter)[1]))
-        updateProgressBar(linkPercent)
+        try:
+            string = str(string)
+            fileScanner(string.split(splitter)[0], linkCleanse(name + "\\" + string.split(splitter)[1]))
+            updateProgressBar(linkPercent)
+        except:
+            continue
 
     tempMover(name)
 
@@ -48,10 +54,14 @@ def fileScanner(innerUrl, name):
 
     links = content.find_elements_by_tag_name('a')
 
+    driver.implicitly_wait(1)
     for link in links:
         if "download" in str(link.get_attribute('href')):
-            driver.implicitly_wait(1)
-            link.click()
+            try:
+                link.click()
+            except:
+                continue
+
             print(link.text + ": " + str(link.get_attribute('href')))
 
 
@@ -98,8 +108,20 @@ def mainMethod():
     os.environ['MOZ_HEADLESS'] = '1'
 
     profile = webdriver.FirefoxProfile()
-    profile.set_preference("browser.helperApps.neverAsk.saveToDisk",
-                           "multipart/x-zip,application/zip,application/x-zip-compressed,application/x-compressed,application/msword,application/csv,text/csv,image/png ,image/jpeg, application/pdf, text/html,text/plain,  application/excel, application/vnd.ms-excel, application/x-excel, application/x-msexcel, application/octet-stream, application/vnd.ms-powerpoint, application/vnd.openxmlformats-officedocument.presentationml.presentation, application/x-rar-compressed, application/rtf,audio/mpeg")
+    profile.set_preference(
+        "browser.helperApps.neverAsk.saveToDisk",
+        "video/x-msvideo,image/bmp,application/x-csh,text/css,text/csv,application/msword,"
+        "application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/epub+zip,"
+        "application/gzip,image/gif,text/html,image/vnd.microsoft.icon,application/java-archive,image/jpeg,"
+        "text/javascript,application/json,audio/midi,audio/x-midi,audio/mpeg,video/mpeg,"
+        "application/vnd.oasis.opendocument.presentation,application/vnd.oasis.opendocument.spreadsheet,"
+        "application/vnd.oasis.opendocument.text,audio/ogg,video/ogg,application/ogg,image/png,application/pdf,"
+        "application/php,application/vnd.ms-powerpoint,"
+        "application/vnd.openxmlformats-officedocument.presentationml.presentation,application/vnd.rar,"
+        "application/rtf,application/x-sh,image/svg+xml,application/x-tar,text/plain,application/vnd.visio,audio/wav,"
+        "audio/webm,video/webm,image/webp,application/xhtml+xml,application/vnd.ms-excel,"
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/xml,text/xml,application/zip,"
+        "video/3gpp,audio/3gpp,video/3gpp2,audio/3gpp2,application/x-7z-compressed,video/mp4")
     profile.set_preference("pdfjs.disabled", True)
     profile.set_preference("browser.download.folderList", 2)
     profile.set_preference("browser.download.manager.showWhenStarting", False)
@@ -138,8 +160,12 @@ def mainMethod():
         courseStrings.append(course.get_attribute('href') + "/modules" + splitter + str(course.text).split("\n")[0])
 
     for string in courseStrings:
-        string = str(string)
-        courseScanner(string.split(splitter)[0], linkCleanse(basedir + string.split(splitter)[1]), len(courseStrings))
+        try:
+            string = str(string)
+            courseScanner(string.split(splitter)[0], linkCleanse(basedir + string.split(splitter)[1]),
+                          len(courseStrings))
+        except:
+            continue
 
     print("Completed")
     print("Find files at " + dir_path + "\\" + basedir)
